bae12f03917f58522706136b932f897cb00e9093
brajm008 <ujkkju117@outlook.com>
2021-07-05 19:40:45 +0930

    Add project files.

---
+++ DiceCallbackPlugin/DiceCallbackPlugin.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using BepInEx;
+
+namespace DiceCallbackPlugin
+{
+
+    [BepInPlugin(Guid, "HolloFoxes' Dice Callback Plug-In", Version)]
+    public class DiceCallbackPlugin : BaseUnityPlugin
+    {
+        // constants
+        private const string Guid = "org.hollofox.plugins.DiceCallbackPlugin";
+        private const string Version = "1.0.0.0";
+
+        /// <summary>
+        /// Awake plugin
+        /// </summary>
+        void Awake()
+        {
+            Debug.Log("DiceCallback Plug-in loaded");
+
+            ModdingTales.ModdingUtils.Initialize(this, Logger);
+        }
+
+        /// <summary>
+        /// Looping method run by plugin
+        /// </summary>
+        void Update()
+        {
+            if (OnBoard())
+            {
+                DiceRoller.CheckDice();
+            }
+        }
+
+        private bool OnBoard()
+        {
+            return (CameraController.HasInstance &&
+                    BoardSessionManager.HasInstance &&
+                    BoardSessionManager.HasBoardAndIsInNominalState &&
+                    !BoardSessionManager.IsLoading);
+        }
+    }
+}
---
+++ DiceCallbackPlugin/DiceRoller.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameChat.UI;
+using Mono.Cecil.Cil;
+using Newtonsoft.Json;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DiceCallbackPlugin
+{
+    public static class DiceRoller
+    {
+        private static Dictionary<string, Action<Dictionary<DiceType, List<int>>>> DiceCallbacks = new Dictionary<string, Action<Dictionary<DiceType, List<int>>>>();
+
+        /// <summary>
+        /// Rolls dice
+        /// </summary>
+        /// <param name="flavor">Name of what you're rolling</param>
+        /// <param name="formula">text formula such as "1d6+5"</param>
+        /// <param name="callback">Callback with results</param>
+        public static void RollDice(string flavor, string formula, Action<Dictionary<DiceType, List<int>>> callback = null)
+        {
+            flavor = flavor.Replace(" ", "_");
+            formula = formula.Replace(" ", "");
+            var id = Guid.NewGuid();
+
+            var title = $"{flavor}<size=0>{id}";
+
+            var command = $"talespire://dice/{title}:{formula}";
+            DiceCallbacks.Add($"{title}", callback);
+
+            Debug.Log(command);
+            System.Diagnostics.Process.Start(command).WaitForExit();
+        }
+
+        /// <summary>
+        /// Rolls dice
+        /// </summary>
+        /// <param name="flavor">Name of what you're rolling</param>
+        /// <param name="formula">Collection of dice to roll</param>
+        /// <param name="callback">Callback with results</param>
+        public static void RollDice(string flavor, Dice formula, Action<Dictionary<DiceType, List<int>>> callback = null)
+        {
+            var textFormula = "";
+            var keys = formula.GetKeys();
+            var first = true;
+            for(var i = 0; i < keys.Length; i++)
+            {
+                var key = keys[i];
+                
+                if (formula.Get(key) != 0)
+                {
+                    if (!first && formula.Get(key) > 0) textFormula += "+";
+                    if (key == DiceType.modifier)
+                    {
+                        textFormula += $"{formula.Get(key)}";
+                    }
+                    else textFormula += $"{formula.Get(key)}{key}";
+                    first = false;
+                }
+            }
+            RollDice(flavor,textFormula, callback);
+        }
+
+        private static string last = "";
+        private static string last_key = "";
+
+        public static void CheckDice()
+        {
+            if (DiceManager.HasInstance)
+            {
+                // var obj = GameObject.Find("GUIManager/PANEL_STRUCTURE/MiddlePanel/MiddlePanel/RightPanel/ContentPanel/Content/#PANEL_Chat/UIChatManager/Scroll View/Viewport/Content/ChatSection(Clone)"); //.GetComponent<UIChatMessageSection>();
+                var obj = GameObject.Find("GUIManager/PANEL_STRUCTURE/MiddlePanel/MiddlePanel/MiddlePanel/MidTopPanel/DiceRollResults"); //.GetComponent<UIChatMessageSection>();
+                if (obj != null)
+                {
+                    var comp = obj.GetComponent<UIDiceRollResult>();
+                    if (comp.isActiveAndEnabled)
+                    {
+                        // Need to detect change as well not just open/close
+                        var TXT = comp.transform.GetChild(1).GetChild(0).GetComponent<TextMeshProUGUI>();
+                        var key = TXT.text;
+                        if (key.Contains("Rolled ") )
+                        {
+                            key = key.Replace("Rolled ", "");
+
+                            var single = comp.transform.GetChild(1).GetChild(1).GetChild(0)
+                                .GetComponent<UIDiceScoreItem>();
+                            var multi = comp.transform.GetChild(1).GetChild(2);
+
+                            Dictionary<DiceType, List<int>> DiceCollection = new Dictionary<DiceType, List<int>>();
+
+                            if (single.isActiveAndEnabled)
+                            {
+                                var r = single.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+                                var t = single.transform.GetComponent<RawImage>();
+                                var d = DiceType.d4;
+                                switch (t.texture.name)
+                                {
+                                    case "1D4":
+                                        d = DiceType.d4;
+                                        break;
+                                    case "1D6":
+                                        d = DiceType.d6;
+                                        break;
+                                    case "1D8":
+                                        d = DiceType.d8;
+                                        break;
+                                    case "1D10":
+                                        d = DiceType.d10;
+                                        break;
+                                    case "1D100":
+                                        d = DiceType.d100;
+                                        break;
+                                    case "1D12":
+                                        d = DiceType.d12;
+                                        break;
+                                    case "1D20":
+                                        d = DiceType.d20;
+                                        break;
+                                }
+                                if (!DiceCollection.ContainsKey(d))
+                                {
+                                    DiceCollection.Add(d, new List<int>());
+                                }
+                                DiceCollection[d].Add(int.Parse(r.text));
+                            }
+                            else
+                            {
+                                var components = multi.GetComponentsInChildren<UIDiceScoreItem>();
+                                foreach (var component in components)
+                                {
+                                    var r = component.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+                                    var t = component.transform.GetComponent<RawImage>();
+                                    var d = DiceType.d4;
+                                    switch (t.texture.name)
+                                    {
+                                        case "1D4":
+                                            d = DiceType.d4;
+                                            break;
+                                        case "1D6":
+                                            d = DiceType.d6;
+                                            break;
+                                        case "1D8":
+                                            d = DiceType.d8;
+                                            break;
+                                        case "1D10":
+                                            d = DiceType.d10;
+                                            break;
+                                        case "1D100":
+                                            d = DiceType.d100;
+                                            break;
+                                        case "1D12":
+                                            d = DiceType.d12;
+                                            break;
+                                        case "1D20":
+                                            d = DiceType.d20;
+                                            break;
+                                    }
+                                    if (!DiceCollection.ContainsKey(d))
+                                    {
+                                        DiceCollection.Add(d, new List<int>());
+                                    }
+                                    DiceCollection[d].Add(int.Parse(r.text));
+                                }
+
+                                var totalComponents = multi.GetComponentsInChildren<UIDiceTotalItem>();
+                                if (totalComponents.Length == 2)
+                                {
+                                    var r = totalComponents[0].gameObject.GetComponent<TextMeshProUGUI>();
+                                    var d = DiceType.modifier;
+                                    if (!DiceCollection.ContainsKey(d))
+                                    {
+                                        DiceCollection.Add(d, new List<int>());
+                                    }
+                                    DiceCollection[d].Add(int.Parse(r.text));
+                                }
+                            }
+                            
+                            if (!key.Equals(last_key) || !last.Equals(JsonConvert.SerializeObject(DiceCollection)))
+                            {
+                                last_key = key;
+                                last = JsonConvert.SerializeObject(DiceCollection);
+                                if (DiceCallbacks.ContainsKey(key)) DiceCallbacks[key](DiceCollection);
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                last = "";
+                last_key = "";
+            }
+        }
+
+        public class Dice
+        {
+            private Dictionary<DiceType, int> DiceCollection = new Dictionary<DiceType, int>();
+            public Dice()
+            {
+                DiceCollection.Add(DiceType.d4,0);
+                DiceCollection.Add(DiceType.d6,0);
+                DiceCollection.Add(DiceType.d8,0);
+                DiceCollection.Add(DiceType.d10,0);
+                DiceCollection.Add(DiceType.d100,0);
+                DiceCollection.Add(DiceType.d12,0);
+                DiceCollection.Add(DiceType.d20,0);
+                DiceCollection.Add(DiceType.modifier,0);
+            }
+            public Dice((DiceType t,int v)[] values)
+            {
+                DiceCollection.Add(DiceType.d4, 0);
+                DiceCollection.Add(DiceType.d6, 0);
+                DiceCollection.Add(DiceType.d8, 0);
+                DiceCollection.Add(DiceType.d10, 0);
+                DiceCollection.Add(DiceType.d100, 0);
+                DiceCollection.Add(DiceType.d12, 0);
+                DiceCollection.Add(DiceType.d20, 0);
+                DiceCollection.Add(DiceType.modifier, 0);
+
+                foreach (var (t, v) in values)
+                {
+                    Set(t,v);
+                }
+            }
+
+            public void Clear()
+            {
+                foreach (var key in GetKeys())
+                {
+                    DiceCollection[key] = 0;
+                }
+            }
+
+            public void Set(DiceType key, int value)
+            {
+                DiceCollection[key] = value;
+            }
+
+            public void Add(DiceType key, int value)
+            {
+                DiceCollection[key] += value;
+            }
+
+            public void Remove(DiceType key, int value)
+            {
+                DiceCollection[key] -= value;
+            }
+
+            public int Get(DiceType key)
+            {
+                return DiceCollection[key];
+            }
+
+            public DiceType[] GetKeys()
+            {
+                return DiceCollection.Keys.ToArray();
+            }
+        }
+
+        public enum DiceType
+        {
+            d4,
+            d6,
+            d8,
+            d10,
+            d100,
+            d12,
+            d20,
+            modifier
+        }
+    }
+}
